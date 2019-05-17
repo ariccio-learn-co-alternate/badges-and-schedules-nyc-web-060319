5f523931e60c615caf240965b183d0d04803bb73
Alexander Riccio <test35965@gmail.com>
2019-05-17 20:20:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ end
 def assign_rooms(attendees)
   assignments = []
   for person in attendees do
-    
+    assignments << "Hello, #{person}! You'll be assigned to room #{assignments.length + 1}!"
   end
 end
